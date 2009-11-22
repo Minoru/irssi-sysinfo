35ed6d4bc1b24d67179a9628eec7767abc124fb3
Batischev Alexandr <eual.jp@gmail.com>
2009-11-22 14:59:55 +0200

    GNU GPL header and copyright notice added

--- sysinfo.pl
+++ sysinfo.pl
@@ -1,5 +1,26 @@
 #!/usr/bin/perl
 
+#
+#  irssi sysinfo script
+#  Written by Alexandr Batischev aka Minoru <eual.jp@gmail.com>
+#  Copyright 2009 Alexandr Batischev
+#
+
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 # To those who will use, support or modify this crap :)
 #
 # This script have (at least, must have :) modal structure.
